ce2144280a9caa20639ae84dc4d875e5b5becea0
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-26 12:13:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function validEmail(str) {
-  //your JS code here.
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (str.trim() === '') {
+    return false;
+  }
+
+  // Check if the email matches the regular expression
+  return emailRegex.test(str);
 }
 
 // Do not change the code below.
